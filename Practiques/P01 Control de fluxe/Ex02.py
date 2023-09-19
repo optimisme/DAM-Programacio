81f123e7c0290d524f3d6d13304c5a0bca6d23c0
optimisme <optimisme@gmail.com>
2023-09-19 09:46:13 +0200

    X

--- Practiques/P01 Control de fluxe/Ex02.py
+++ Practiques/P01 Control de fluxe/Ex02.py
@@ -5,7 +5,7 @@ Un programa ha de llegir un any per teclat i ha de dir si és un any de traspàs
 
 Per calcular si un any és de traspàs és: 
 
-- El residu (mòdul) de l'any dividit entre 4 ha de ser 0
+- El residu (mòdul) de l'any dividit entre 4 ha de ser 0 
 
 - A més: el residu de l'any entre 100 ha de ser diferent de 0 o el residu de l'any entre 400 ha de ser 0
 
