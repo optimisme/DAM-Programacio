fcabdebacdf6be1f0661bf867283de1c0dea62ba
optimisme <optimisme@gmail.com>
2023-09-19 10:06:41 +0200

    X

---
+++ Practiques/P01 Control de fluxe/Ex06.py
@@ -0,0 +1,22 @@
+"""
+Exercici 6
+
+Crea un programa per validar contrasenyes. Aquestes han de complir:
+
+- Tenir almenys 8 caràcters
+
+- Tenir lletres minúscules, majúscules, i almenys un símbol (punt, coma, interrogant, exclamació)
+
+- No pot contenir espais en blanc
+
+La validació la farà una funció 'validaContrasenya' que retornarà True o False.
+
+Al inici, demanarà introduir una contrasenya.
+
+La sortida del programa ha de ser:
+
+- Si la contrasenya no compleix alguna de les condicions: "Contrasenya incorrecta"
+
+- Si la contrasenya compleix totes les condicions: "Contrasenya correcta"
+
+"""
