6a092b54c4703e12d46cb4ff65adaf587eaf743a
optimisme <optimisme@gmail.com>
2023-09-19 09:44:50 +0200

    X

--- Practiques/P01 Control de fluxe/Ex02.py
+++ Practiques/P01 Control de fluxe/Ex02.py
@@ -7,7 +7,7 @@ Per calcular si un any és de traspàs és:
 
 - El residu (mòdul) de l'any dividit entre 4 ha de ser 0
 
-- A més, el residu de l'any entre 100 ha de ser diferent de 0 o el residu de l'any entre 400 ha de ser 0
+- A més: el residu de l'any entre 100 ha de ser diferent de 0 o el residu de l'any entre 400 ha de ser 0
 
 Per exemple:
 
