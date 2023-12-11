00bd7a31bf4d25244bd56ef1f957cb6103c80229
optimisme <optimisme@gmail.com>
2023-12-11 15:56:17 +0100

    .

---
+++ Teoria/imports_extra.py
@@ -0,0 +1,10 @@
+
+llista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def sumaTres(a, b, c):
+    return a + b + c
+
+noms = ["Joan", "Maria", "Pere", "Anna", "Pau", "Laia", "Jordi", "Marta", "Pol"]
+
+def comptaLletres(nom):
+    return len(nom)
