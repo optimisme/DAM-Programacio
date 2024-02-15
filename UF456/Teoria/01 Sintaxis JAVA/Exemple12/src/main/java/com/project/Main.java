c81e8c3e1ad539b6ff28977e6bcf309c941e91a8
optimisme <optimisme@gmail.com>
2024-02-15 16:10:18 +0100

    .

--- UF456/Teoria/01 Sintaxis JAVA/Exemple12/src/main/java/com/project/Main.java
+++ UF456/Teoria/01 Sintaxis JAVA/Exemple12/src/main/java/com/project/Main.java
@@ -6,6 +6,8 @@ import java.nio.file.Paths;
 
 import java.util.Scanner;
 
+public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double suma = 0.0;
