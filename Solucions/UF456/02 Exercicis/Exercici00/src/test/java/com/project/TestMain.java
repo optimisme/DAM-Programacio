cd97af07c8ec07b5ab294f21a07bab06be22e586
optimisme <optimisme@gmail.com>
2024-02-28 19:15:30 +0100

    .

---
+++ Solucions/UF456/02 Exercicis/Exercici00/src/test/java/com/project/TestMain.java
@@ -0,0 +1,26 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.github.stefanbirkner.systemlambda.SystemLambda;
+
+public class TestMain {
+
+    @Test
+    public void testMainOutput() throws Exception {
+        String text = SystemLambda.tapSystemOut(() -> {
+            // Executa el main per a provar la seva sortida
+            String[] args = {}; // Passa els arguments necessaris si n'hi ha
+            Main.main(args);
+        });
+
+        // Comprova que la sortida conté el text esperat
+        String expectedOutput = "El resultat de la divisió és 21.015625\nEl resultat arrodonit de la divisió és 21,02";
+        assertTrue(text.contains(expectedOutput), 
+            ">>>>>>>>>> >>>>>>>>>>\n" +
+            "El missatge de sortida no coincideix amb l'esperat. \n" +
+            "Esperat: \n" + expectedOutput + "\n" + 
+            "Obtingut: \n" + text + 
+            "<<<<<<<<<<< <<<<<<<<<<\n");
+    }
+}
