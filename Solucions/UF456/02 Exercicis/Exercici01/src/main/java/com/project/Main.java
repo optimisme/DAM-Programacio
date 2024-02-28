7bb5c2f88b4d3736c349bc1f4068d1ee399e5b19
optimisme <optimisme@gmail.com>
2024-02-28 19:25:41 +0100

    .

--- Solucions/UF456/02 Exercicis/Exercici01/src/main/java/com/project/Main.java
+++ Solucions/UF456/02 Exercicis/Exercici01/src/main/java/com/project/Main.java
@@ -1,7 +1,17 @@
 package com.project;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        System.out.print("Quina edat tens? ");
+
+        String input = scanner.nextLine();
+        int numero = Integer.parseInt(input) * 7;
+
+        System.out.println("Si fossis un gos, tindries " + numero + " anys");
         
+        scanner.close(); 
     }
 }
