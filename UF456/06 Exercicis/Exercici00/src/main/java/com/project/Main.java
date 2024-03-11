6815a079645caff9a36fa614adb225519cd81acc
optimisme <optimisme@gmail.com>
2024-03-11 16:02:09 +0100

    .

--- UF456/06 Exercicis/Exercici00/src/main/java/com/project/Main.java
+++ UF456/06 Exercicis/Exercici00/src/main/java/com/project/Main.java
@@ -2,22 +2,5 @@ package com.project;
 
 
 public class Main {
-    public static void main(String[] args) {
-        Cercle cercle = new Cercle(5);
-        System.out.println("Cercle amb radi 5:");
-        System.out.println("Àrea: " + cercle.calcularArea());
-        System.out.println("Perímetre: " + cercle.calcularPerimetre());
 
-        // Creem un rectangle amb amplada 4 i altura 5
-        Rectangle rectangle = new Rectangle(4, 5);
-        System.out.println("\nRectangle amb amplada 4 i altura 5:");
-        System.out.println("Àrea: " + rectangle.calcularArea());
-        System.out.println("Perímetre: " + rectangle.calcularPerimetre());
-
-        // Creem un triangle amb costats 3, 4, 5
-        Triangle triangle = new Triangle(3, 4, 5);
-        System.out.println("\nTriangle amb costats 3, 4, 5:");
-        System.out.println("Àrea: " + triangle.calcularArea());
-        System.out.println("Perímetre: " + triangle.calcularPerimetre());
-    }
 }
