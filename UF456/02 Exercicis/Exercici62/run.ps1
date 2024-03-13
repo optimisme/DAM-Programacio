249f138ea48037e95b9ec0be0f9fdad6ace69dc6
optimisme <optimisme@gmail.com>
2024-03-13 19:00:43 +0100

    .

--- UF456/02 Exercicis/Exercici62/run.ps1
+++ UF456/02 Exercicis/Exercici62/run.ps1
@@ -3,11 +3,14 @@
 # Exemple de funcionament: .\run.ps1 com.project.Main
 # on 'Main' és la classe amb 'main' que volem executar
 
+# Set console output to UTF-8
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 # Change to the directory where the script is located
 Set-Location $PSScriptRoot
 
 # Set MAVEN_OPTS environment variable
-$env:MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED"
+$env:MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED -Dfile.encoding=UTF8"
 
 # Check for the first argument and set it as the main class
 $mainClass = $args[0]
