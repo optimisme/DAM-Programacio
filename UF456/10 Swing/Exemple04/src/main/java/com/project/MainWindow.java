4593956a4b710f4ce46a9195db6faab915ff44e9
optimisme <optimisme@gmail.com>
2024-04-08 17:02:49 +0200

    .

---
+++ UF456/10 Swing/Exemple04/src/main/java/com/project/MainWindow.java
@@ -0,0 +1,103 @@
+package com.project;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    
+    private JButton openButton;
+    private JLabel imageLabel;
+    private JTextArea textArea;
+    private JScrollPane scrollPane;
+
+    public MainWindow() {
+
+        // Títol i mida de la finestra
+        super("SWING Exemple 04");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+   
+        // Definir els elements del panel
+        initComponents();   
+    }
+
+    private void initComponents () {
+
+        openButton = new JButton("Obre Arxiu");
+        openButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                chooseFile();
+            }
+        });
+
+        imageLabel = new JLabel();
+        textArea = new JTextArea(20, 50);
+        textArea.setEditable(false);
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setVisible(false);
+
+        add(openButton);
+        add(imageLabel);
+        add(scrollPane);
+    }
+
+    private void chooseFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            displayFile(selectedFile);
+        }
+    }
+
+    private void displayFile(File file) {
+        try {
+            if (isImageFile(file)) {
+                BufferedImage image = ImageIO.read(file);
+                ImageIcon icon = new ImageIcon(image.getScaledInstance(-1, 300, Image.SCALE_SMOOTH));
+                imageLabel.setIcon(icon);
+                scrollPane.setVisible(false);
+                imageLabel.setVisible(true);
+            } else if (isTextFile(file)) { 
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                textArea.setText(""); // Netejar el textArea abans de carregar nou contingut.
+                textArea.read(reader, null);
+                reader.close();
+                imageLabel.setIcon(null); // Això neteja l'última imatge mostrada.
+                imageLabel.setVisible(false);
+                scrollPane.setVisible(true);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private boolean isTextFile(File file) {
+        String[] imageExtensions = new String[]{"txt", "xml", "java", "py", "yaml", "json"};
+        for (String extension : imageExtensions) {
+            if (file.getName().toLowerCase().endsWith(extension)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isImageFile(File file) {
+        String[] imageExtensions = new String[]{"png", "jpg", "jpeg", "gif", "bmp"};
+        for (String extension : imageExtensions) {
+            if (file.getName().toLowerCase().endsWith(extension)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
