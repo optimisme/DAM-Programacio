fe67a5f700aad10dee5b09870f6b5968dbe3aec6
optimisme <optimisme@gmail.com>
2024-04-17 12:03:15 +0200

    .

--- Solucions/UF456/04 Exercicis/Exercici06/src/test/java/com/project/TestMain.java
+++ Solucions/UF456/04 Exercicis/Exercici06/src/test/java/com/project/TestMain.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Modifier;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.github.stefanbirkner.systemlambda.SystemLambda;
 
