abbca1d4a5156d9f1aa8853ba6c59ed1453073d6
optimisme <optimisme@gmail.com>
2024-04-17 12:01:05 +0200

    Fix returning labels

--- Solucions/UF456/09 Exercicis/Exercici03/src/main/java/com/project/AppData.java
+++ Solucions/UF456/09 Exercicis/Exercici03/src/main/java/com/project/AppData.java
@@ -94,7 +94,7 @@ class AppData {
             while (rs.next()) {
                 Map<String, Object> row = new HashMap<>();
                 for (int i = 1; i <= columnCount; i++) {
-                    row.put(metaData.getColumnName(i), rs.getObject(i));
+                    row.put(metaData.getColumnLabel(i), rs.getObject(i));
                 }
                 resultList.add(row);
             }
