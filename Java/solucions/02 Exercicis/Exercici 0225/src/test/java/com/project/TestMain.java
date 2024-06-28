8de681d4b86686fcaed54437b14d109115db7582
optimisme <optimisme@gmail.com>
2024-06-28 07:18:15 +0200

    .

--- Java/solucions/02 Exercicis/Exercici 0225/src/test/java/com/project/TestMain.java
+++ Java/solucions/02 Exercicis/Exercici 0225/src/test/java/com/project/TestMain.java
@@ -2,9 +2,10 @@ package com.project;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import com.github.stefanbirkner.systemlambda.SystemLambda;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Locale;
 
 public class TestMain {
@@ -38,17 +39,27 @@ public class TestMain {
                 String input = inputs[i];
                 String expectedOutput = expectedOutputs[i];
 
-                String actualOutput = SystemLambda.tapSystemOut(() -> {
-                    System.setIn(new ByteArrayInputStream(input.getBytes())); // Simula l'entrada de l'usuari
-                    Main.main(new String[]{});
-                });
+                // Redirigeix la sortida estàndard a un flux de sortida
+                ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+                PrintStream originalOut = System.out;
+                System.setOut(new PrintStream(outContent));
 
-                actualOutput = actualOutput.replace("\r\n", "\n"); // Normalitza les noves línies
+                // Simula l'entrada de l'usuari
+                System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+                // Executa el main per a provar la seva sortida
+                Main.main(new String[]{});
+
+                // Comprova que la sortida conté el text esperat
+                String actualOutput = outContent.toString().replace("\r\n", "\n"); // Normalitza les noves línies
                 String diff = TestStringUtils.findFirstDifference(actualOutput, expectedOutput);
                 assertTrue(diff.compareTo("identical") == 0,
                     "\n>>>>>>>>>> >>>>>>>>>>\n" +
                     diff +
                     "<<<<<<<<<< <<<<<<<<<<\n");
+
+                // Restaura els fluxos originals
+                System.setOut(originalOut);
             }
         } finally {
             Locale.setDefault(defaultLocale); // Restaura la configuració regional per defecte
