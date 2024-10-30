d1689a9cd5f351568978a246cd3d47cfbd2b6b3c
optimisme <optimisme@gmail.com>
2024-10-30 15:51:13 +0100

    .

--- Python/52 Deures D0/exercici024resolt.py
+++ Python/52 Deures D0/exercici024resolt.py
@@ -56,7 +56,7 @@ def app_run():
 
     now = datetime.now()
     current_time_ms = now.timestamp() * 1000
-    
+
     # Hores amb fracció de minuts (format 12 hores)
     time["hours"] = (current_time_ms / 3600000) % 12
 
@@ -85,27 +85,27 @@ def app_draw():
         label_rect = label.get_rect(center=(x, y))  # Centrar el text
         screen.blit(label, label_rect)
     
-    # Constants
-    offset = -90 # Ajust per rotar l'angle l'origen d'un rellotge
+    # Ajust per rotar l'angle d'origen pygame
+    # a l'angle 0 d'un rellotge
+    offset = -90 
 
-    # Càlcul de l'angle de les hores (continu)
+    # Càlcul de l'angle de les hores 
     degrees_per_hour = (360 / 12)
     hour_angle = degrees_per_hour * time["hours"] + offset
     hour_x, hour_y = utils.point_on_circle(center, radius * 0.4, hour_angle)
     pygame.draw.line(screen, WHITE, center, (hour_x, hour_y), 10)
 
-    # Càlcul de l'angle dels minuts (continu)
+    # Càlcul de l'angle dels minuts 
     degress_per_min = (360 / 60)
     minute_angle = degress_per_min * time["minutes"] + offset
     minute_x, minute_y = utils.point_on_circle(center, radius * 0.7, minute_angle)
     pygame.draw.line(screen, BLUE, center, (minute_x, minute_y), 6)
 
-    # Càlcul de l'angle dels segons (continu)
+    # Càlcul de l'angle dels segons 
     degress_per_sec = (360 / 60)
     second_angle = degress_per_sec * time["seconds"] + offset
     second_x, second_y = utils.point_on_circle(center, radius * 0.9, second_angle)
     pygame.draw.line(screen, RED, center, (second_x, second_y), 2)
-
 
     pygame.display.update()
 
