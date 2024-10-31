4b5acbaeb79c3ef841004b0f43ae8c5eb8d56766
Albert <optimisme@gmail.com>
2024-10-31 10:33:29 +0100

    .

--- Python/53 Deures D1/exercici004resolt.py
+++ Python/53 Deures D1/exercici004resolt.py
@@ -1,4 +1,4 @@
-import math
+import random
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
@@ -8,11 +8,13 @@ import utils
 # Definir colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GRAY = (200, 200, 200)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-BLUE = (50, 150, 255)
-RED = (255, 0, 0)
+GREEN = (127, 184, 68)
+YELLOW = (240, 187, 64)
+ORANGE = (226, 137, 50)
+RED = (202, 73, 65)
+PURPLE = (135, 65, 152)
+BLUE  = (75, 154, 217)
+colors = [GREEN, YELLOW, ORANGE, RED, PURPLE, BLUE]
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -23,7 +25,7 @@ pygame.display.set_caption('Window Title')
 
 # Variables globals
 mouse_pos = { "x": -1, "y": -1 }
-finestra = { "ample": 0, "alt": 0, "mig_x": 0, "mig_y": 0 }
+dots = []
 
 # Bucle de l'aplicació
 def main():
@@ -55,24 +57,38 @@ def app_events():
             else:
                 mouse_pos["x"] = -1
                 mouse_pos["y"] = -1
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            dots.append({ 
+                "x": mouse_pos["x"],
+                "y": mouse_pos["y"],
+                "radius": 25,
+                "color": random.choice(colors)
+            })
     return True
 
 # Fer càlculs
 def app_run():
-    global finestra
+    global dots
 
-    finestra["ample"] = screen.get_width()
-    finestra["alt"] = screen.get_height()
-    finestra["mig_x"] = int(screen.get_width() / 2)
-    finestra["mig_y"] = int(screen.get_height() / 2)
+    delta_time = clock.get_time() / 1000.0  # Convertir a segons
+
+    speed = 5 
+
+    for dot in dots:
+        dot["radius"] = dot["radius"] - speed * delta_time
+        if dot["radius"] < 5:
+            dot["radius"] = 5
 
 # Dibuixar
 def app_draw():
     screen.fill(WHITE)
     utils.draw_grid(pygame, screen, 50)
     
-    # Dibuixar botons
-    
+    # Dibuixar punts
+    for dot in dots:
+        center = (dot["x"], dot["y"])
+        pygame.draw.circle(screen, dot["color"], center, dot["radius"])
+
     
     pygame.display.update()
 
