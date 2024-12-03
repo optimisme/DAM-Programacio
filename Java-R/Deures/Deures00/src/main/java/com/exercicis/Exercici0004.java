285d71ebabe125bb4fbff76f5f9040c721cec7a8
optimisme <optimisme@gmail.com>
2024-12-03 19:03:39 +0100

    .

--- Java-R/Deures/Deures00/src/main/java/com/exercicis/Exercici0004.java
+++ Java-R/Deures/Deures00/src/main/java/com/exercicis/Exercici0004.java
@@ -19,18 +19,14 @@ public class Exercici0004 {
     }
 
     public static String validaContrasenya(String contrasenya) {
-        if (contrasenya.length() >= 8 &&
-            contaMajuscules(contrasenya) >= 2 &&
-            contaMinuscules(contrasenya) >= 2) {
-            return "La contrasenya és vàlida";
-        } else {
-            return "La contrasenya NO és vàlida";
-        }
+        /*
+            TODO: Resol aquí la funció
+        */
+        return "";
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         /*
             TODO: Resol aquí l'exercici
         */
