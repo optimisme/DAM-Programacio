3629be29742a1b2ae1c1a8b594f5c9b8f30b2f7e
optimisme <optimisme@gmail.com>
2024-12-04 09:31:29 +0100

    .

--- Java-R/Deures/Deures00/src/main/java/com/exercicis/Exercici0005.java
+++ Java-R/Deures/Deures00/src/main/java/com/exercicis/Exercici0005.java
@@ -4,55 +4,22 @@ package com.exercicis;
 public class Exercici0005 {
 
     public static boolean isPalindrom(String text) {
-        // Normalitzar text
-        text = normalize(text);
-        
-        // Invertir el text
-        int lastLetter = text.length() - 1;
-        String inverted = "";
-        for (int cnt = lastLetter; cnt >= 0; cnt = cnt - 1) {
-            String letter = String.valueOf(text.charAt(cnt));
-            inverted = inverted + letter;
-        }
-
-        return text.equals(inverted);
+        /*
+            TODO: Resol aquí la funció
+        */
+        return false;
     }
 
     public static String normalize(String text) {
-        String rst = text.toLowerCase();
-    
-        // Arrays amb caràcters especials i les seves substitucions
-        String[] accents = {"à", "á", "è", "é", "í", "ò", "ó", "ú", "ù", " ", "'", "!", "\\.", ",", "·"};
-        String[] senseAccents = {"a", "a", "e", "e", "i", "o", "o", "u", "u", "", "", "", "", "", ""};
-    
-        // Substituir cada caràcter del text original
-        for (int cnt = 0; cnt < accents.length; cnt++) {
-            rst = rst.replaceAll(accents[cnt], senseAccents[cnt]);
-        }
-        
-        return rst;
+        /*
+            TODO: Resol aquí la funció
+        */
+        return "";
     }
-    
    
     public static void main(String[] args) {
-
-        String[] exemples = {
-            "Anul·la la lluna",
-            "Atrapa la lluna",
-            "Atrapa'l o l'aparta",
-            "Aparta'l o atrapa'l",
-            "No sap pas on",
-            "On sap pas qui",
-            "Tramaran anar a Mart",
-            "A Mart trobaràn art",
-            "Un pop nu",
-            "Nu pop un"
-        };
-
-        // Comprovar cada text
-        for (String text : exemples) {
-            boolean esPalindrom = isPalindrom(text);
-            System.out.println(text + " (" + (esPalindrom ? "Si" : "No") + ")");
-        }
+        /*
+            TODO: Resol aquí l'exercici
+        */
     }
 }
