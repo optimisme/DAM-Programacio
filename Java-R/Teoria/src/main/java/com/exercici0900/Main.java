0359f0f95e4efc6bea80c490be0d0119865a5a67
optimisme <optimisme@gmail.com>
2024-12-05 16:49:15 +0100

    Added 900-909

---
+++ Java-R/Teoria/src/main/java/com/exercici0900/Main.java
@@ -0,0 +1,10 @@
+package com.exercici0900;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        /* TODO: 
+           Fes aquí el codi de l'exercici
+        */
+    }
+}
