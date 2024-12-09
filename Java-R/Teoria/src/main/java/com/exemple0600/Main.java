bb4fbf7e825744197d829a9102b07ef53728fdeb
optimisme <optimisme@gmail.com>
2024-12-09 17:58:55 +0100

    .

--- Java-R/Teoria/src/main/java/com/exemple0600/Main.java
+++ Java-R/Teoria/src/main/java/com/exemple0600/Main.java
@@ -1,10 +1,15 @@
 package com.exemple0600;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         // Declaració i inicialització d'un array
         int[] numeros = {10, 20, 30, 40, 50};
+
+        // Escriure els elements
+        System.out.println("Array: " + Arrays.toString(numeros));
 
         // Accés a elements
         System.out.println("Element a l'índex [0]: " + numeros[0]);
