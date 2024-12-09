60381ca003398dc589b62b8b918971dbac6be004
optimisme <optimisme@gmail.com>
2024-12-09 16:15:51 +0100

    .

--- Java-R/Teoria/src/main/java/com/exercici0502/Main.java
+++ Java-R/Teoria/src/main/java/com/exercici0502/Main.java
@@ -1,41 +1,10 @@
 package com.exercici0502;
 
-import java.util.Scanner;
-
 public class Main {
-
-    public static float maxOfThree(float a, float b, float c){
-        if (a>b){
-            if (a>c){
-                return a;
-            }
-            else{
-                return c;
-            }
-        }
-        else if (b>c){
-            return b;
-        }
-        else{
-            return c;
-        }
-    }
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
         
-        System.out.println("Introduce un numero(a): ");
-        float a = sc.nextInt();
-
-        System.out.println("Introduce otro numero(b): ");
-        float b = sc.nextInt();
-
-        System.out.println("Introduce otro numero(c): ");
-        float c = sc.nextInt();
-
-        float resultado = maxOfThree(a, b, c);
-        System.out.println(resultado);
-
-        sc.close();
+        /* TODO: 
+           Fes aquí el codi de l'exercici
+        */
     }
 }
--- Java-R/Teoria/src/main/java/com/exercici0503/Main.java
+++ Java-R/Teoria/src/main/java/com/exercici0503/Main.java
@@ -1,30 +1,10 @@
 package com.exercici0503;
 
-import java.util.Scanner;
 public class Main {
-
-    public static Boolean startsWithVowel(String word){
-
-        String word_lower = word.toLowerCase();
-
-        if (word_lower.startsWith("a") || word_lower.startsWith("e") || word_lower.startsWith("i") || word_lower.startsWith("o") || word_lower.startsWith("u")){
-            System.out.println("La paraula "+word+" comença per vocal");
-            return true;
-        } else {
-            System.out.println("La paraula "+word+" NO comença per vocal");
-            return false;
-        }
-        
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Paraula: ");
-        String paraula = scanner.nextLine();
-
-        System.out.println(startsWithVowel(paraula));
-        scanner.close();
         
+        /* TODO: 
+           Fes aquí el codi de l'exercici
+        */
     }
 }
