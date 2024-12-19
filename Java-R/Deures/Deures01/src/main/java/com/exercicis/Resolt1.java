77871d6e9a3e1b5141338c26e74436497777c797
optimisme <optimisme@gmail.com>
2024-12-19 16:57:11 +0100

    .

---
+++ Java-R/Deures/Deures01/src/main/java/com/exercicis/Resolt1.java
@@ -0,0 +1,26 @@
+package com.exercicis;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+public class Resolt1 {
+
+
+    /**
+     * 
+     * @run ./run.sh "com.exercicis.Exercici1"
+     * @test ./runTest.sh "com.exercicis.TestExercici1"
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+    }
+}
