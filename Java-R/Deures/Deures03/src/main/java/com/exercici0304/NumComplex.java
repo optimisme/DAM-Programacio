92c94c8ae07ac78226a1d08a20e4597343d4498e
optimisme <optimisme@gmail.com>
2025-01-30 10:46:40 +0100

    .

--- Java-R/Deures/Deures03/src/main/java/com/exercici0304/NumComplex.java
+++ Java-R/Deures/Deures03/src/main/java/com/exercici0304/NumComplex.java
@@ -1,5 +1,55 @@
 package com.exercici0304;
 
 public class NumComplex {
-    
+    private double partReal;
+    private double partImaginaria;
+
+    public NumComplex(double partReal, double partImaginaria) {
+        
+    }
+
+    public double getPartReal() {
+        return 0.0;
+    }
+
+    public void setPartReal(double partReal) {
+        
+    }
+
+    public double getPartImaginaria() {
+        return 0.0;
+    }
+
+    public void setPartImaginaria(double partImaginaria) {
+
+    }
+
+    public double modul() {
+        return 0.0;
+    }
+
+    public NumComplex conjugat() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public static NumComplex suma(NumComplex a, NumComplex b) {
+        return null;
+    }
+
+    public static NumComplex resta(NumComplex a, NumComplex b) {
+        return null;
+    }
+
+    public static NumComplex multiplica(NumComplex a, NumComplex b) {
+        return null;
+    }
+
+    public static NumComplex divideix(NumComplex a, NumComplex b) {
+        return null;
+    }
 }
