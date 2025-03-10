92c4c69214aa25f46b1da312dac36fbd3bb881f6
optimisme <optimisme@gmail.com>
2025-03-10 16:31:22 +0100

    .

--- Java-R/Teoria/src/main/java/com/exercici1400/Biblioteca.java
+++ Java-R/Teoria/src/main/java/com/exercici1400/Biblioteca.java
@@ -1,7 +1,7 @@
 package com.exercici1400;
 
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class Biblioteca {
