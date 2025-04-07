416fd96668229f74fd0bbd541210027466f0d65d
optimisme <optimisme@gmail.com>
2025-04-07 18:24:27 +0200

    .

--- Java-R/Deures/Deures06/src/main/java/com/exercici0602/BuildDatabase.java
+++ Java-R/Deures/Deures06/src/main/java/com/exercici0602/BuildDatabase.java
@@ -1,10 +1,6 @@
 package com.exercici0602;
 
-import com.utils.*;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
